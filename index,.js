60b5f037585d8b8237bbb5ed1c854ad9845cd52c
gubetiati <gubetiati@gmail.com>
2024-03-14 19:12:12 -0300

    comment

--- index,.js
+++ index,.js
@@ -1,9 +1,11 @@
+//teste das classes
+
 const Aluno = require('./aluno')
 const Turma = require('./turma')
 const TurmaPresencial = require('./turmaPresencial')
 
 const aluno = new Aluno('Gustavo', 'gustavoferreira', '2268884')
-const turma = new Turma('ES61', 5)
+const turma = new Turma('ES61', 6)
 const turmaPresencial = new TurmaPresencial('ES61', 7, 80)
 
 console.log(`Aluno: ${aluno.nome}\nRA: ${aluno.RA}\n`)
